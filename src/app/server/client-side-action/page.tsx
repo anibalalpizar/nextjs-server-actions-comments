66f4d50ae0095dabe8ff987e6fde2c3aadbc5fc7
anibalalpizar <anibal.alpizar14@gmail.com>
2025-04-21 23:34:33 -0600

    Add Page component to display comments and comment form with loading state

---
+++ src/app/server/client-side-action/page.tsx
@@ -0,0 +1,44 @@
+import { Suspense } from "react"
+import Link from "next/link"
+import { ArrowLeft, Loader2 } from "lucide-react"
+import { db } from "@/lib/db"
+import { delay } from "@/lib/utils"
+
+import { Comment } from "../../comments/comment"
+import CommentForm from "./comment-form"
+
+export default function Page() {
+  return (
+    <main className="mx-auto max-w-xl space-y-4 p-4">
+      <Link
+        href="/"
+        className="text-primary hover:underline text-sm flex items-center gap-2"
+      >
+        <ArrowLeft className="w-4 h-4" /> Back to Home
+      </Link>
+      <h2 className="text-xl font-bold">Comments</h2>
+      <CommentForm />
+      <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
+        <CommentsList />
+      </Suspense>
+    </main>
+  )
+}
+
+async function CommentsList() {
+  // Artificial delay to pretend we're fetching from a database
+  await delay(2000)
+
+  const comments = await db.findComments({
+    take: 10,
+    sort: "desc",
+  })
+
+  return (
+    <div className="space-y-3">
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </div>
+  )
+}
