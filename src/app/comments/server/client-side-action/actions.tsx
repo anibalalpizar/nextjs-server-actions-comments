f815000f4f4609624b69bdac9ded92c0cc0ea695
anibalalpizar <anibal.alpizar14@gmail.com>
2025-04-28 23:01:58 -0600

    Refactor createComment function to use schema validation for input

--- src/app/comments/server/client-side-action/actions.tsx
+++ src/app/comments/server/client-side-action/actions.tsx
@@ -1,12 +1,16 @@
 "use server"
 
+import { revalidatePath } from "next/cache"
+import { createCommentSchema, CreateCommentValues } from "@/app/comments/schema"
+
 import { User } from "@/lib/auth"
 import { db } from "@/lib/db"
 import { delay } from "@/lib/utils"
-import { revalidatePath } from "next/cache"
 
-export async function createComment(text: string, user: User) {
+export async function createComment(input: CreateCommentValues, user: User) {
   await delay(700)
+  const { text } = createCommentSchema.parse(input)
+
   await db.createComment({
     text,
     user,
