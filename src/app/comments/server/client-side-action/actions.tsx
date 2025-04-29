388667cf9593de57fb8c84cc73700f5db4f14109
anibalalpizar <anibal.alpizar14@gmail.com>
2025-04-28 23:04:15 -0600

    Refactor createComment function to retrieve current user internally

--- src/app/comments/server/client-side-action/actions.tsx
+++ src/app/comments/server/client-side-action/actions.tsx
@@ -3,17 +3,22 @@
 import { revalidatePath } from "next/cache"
 import { createCommentSchema, CreateCommentValues } from "@/app/comments/schema"
 
-import { User } from "@/lib/auth"
+import { getCurrentUser } from "@/lib/auth"
 import { db } from "@/lib/db"
 import { delay } from "@/lib/utils"
 
-export async function createComment(input: CreateCommentValues, user: User) {
+export async function createComment(input: CreateCommentValues) {
   await delay(700)
   const { text } = createCommentSchema.parse(input)
 
+  const currentUser = await getCurrentUser()
+  if (!currentUser) {
+    throw new Error("You must be logged in to post a comment")
+  }
+
   await db.createComment({
     text,
-    user,
+    user: currentUser,
   })
 
   revalidatePath("/comments/server/client-side-action")
